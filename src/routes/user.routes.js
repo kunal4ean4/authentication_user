bde0d01e4c9c0735f32807e284adeb68303d3a63
Kunal Kumar <kunal.kumar@publicisproduction.com>
2026-06-08 22:50:23 +0530

    add multer middleware for profile picture upload in user registration route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-
+import { upload } from "../middlewares/multer.middlewares.js";
 const router=Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields(
+        [
+            {accountPicture:"profilePicture", maxCount:1}
+        ]
+    )
+    ,registerUser)
 export default router;
