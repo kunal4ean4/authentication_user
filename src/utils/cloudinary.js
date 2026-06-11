573d880849205fec90d4b86f52b077546fb6e011
Kunal Kumar <kunal.kumar@publicisproduction.com>
2026-06-11 22:57:52 +0530

    fix: add logging for Cloudinary configuration and error handling

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,6 +2,9 @@ import { v2 as cloudinary } from 'cloudinary';
 import { response } from 'express';
 import fs from "fs"
 // Configuration
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+console.log("API Secret:", process.env.CLOUDINARY_API_SECRET);
     cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY, 
@@ -19,6 +22,7 @@ import fs from "fs"
             console.log("File is uploaded on cloudinary", (await response).url )
             return response
         } catch (error) {
+                console.log("Cloudinary Error:", error);
             fs.unlinkSync(localFilePath)
             // remove the locally saved temporary file as the upload operation got failed
             return null
