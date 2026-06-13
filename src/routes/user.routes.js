f74d5aed3816d19d1161f938977e810e4f5fe42b
Kunal Kumar <kunal.kumar@publicisproduction.com>
2026-06-13 21:52:44 +0530

    feat: add login, logout, and refresh token routes with JWT verification

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser,loginUser,logOut,refreshAccessToken } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const router=Router();
 
 router.route("/register").post(
@@ -10,4 +11,13 @@ router.route("/register").post(
         ]
     )
     ,registerUser)
+
+router.route("/login").post(loginUser)
+
+//Secured routes
+router.route("/logout").post(verifyJWT,logOut)
+router.route("/refresh-token").post(refreshAccessToken)
+
+
+
 export default router;
