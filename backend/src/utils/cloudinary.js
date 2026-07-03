9743b1c7b26bca3209e44eb2741850e1042096d7
Kunal Kumar <kunal.kumar@publicisproduction.com>
2026-07-03 15:47:00 +0530

    feat: add cloudinary utility for file uploads

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { response } from 'express';
+import fs from "fs"
+// Configuration
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+console.log("API Secret:", process.env.CLOUDINARY_API_SECRET);
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+
+
+    const uploadOnCloudinary= async(localFilePath)=>{
+        try {
+            if(!localFilePath) return null
+            // upload the file on cloudinary
+            const response=await cloudinary.uploader.upload(localFilePath,{resource_type:"auto"})
+
+            fs.unlinkSync(localFilePath)
+            return response
+        } catch (error) {
+                console.log("Cloudinary Error:", error);
+            fs.unlinkSync(localFilePath)
+            // remove the locally saved temporary file as the upload operation got failed
+            return null
+        }
+    }
+
+    export {uploadOnCloudinary}
